4e4c4e00d3b0fd2053bb629f7c5e88cdbd178389
StudentSpbPack <71229381+StudentSpbPack@users.noreply.github.com>
2020-10-22 00:40:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework_do
+{
+    class Program
+    {
+        /// <summary>
+        /// The execution method of our code
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Creating a game mode selection
+            Console.WriteLine("Choose the game mode: 1 - 1vs1, 2 - vs PC");
+            byte gamemode;
+            gamemode = Convert.ToByte(Console.ReadLine());
+
+            // Create enter nicknames for the first player and, in the appropriate game mode, for the second player
+            Console.WriteLine("\nPlayer #1, write your nickname");
+            string player1 = Console.ReadLine();
+            string player2 = "PC";
+            if (gamemode == 1)
+            {
+                Console.WriteLine("Player #2, write your nickname");
+                player2 = Console.ReadLine();
+            }
+
+            // Creating a subtraction number variable for the player and number of step
+            int userTry;
+            byte NumbOfStep = 1;
+
+            // Create a subtraction number span
+            Console.WriteLine("\nEnter the initial limit of the number of subtractions for the player");
+            int earlynumberTry = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the final limit of the number of subtractions for the player");
+            int endnumberTry = Convert.ToInt32(Console.ReadLine());
+
+            // Creating limits for generating a game number
+            Console.WriteLine("\nEnter the number from which the game number will be generated");
+            int earlynumberGameNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the number up to which the game number will be generated");
+            int endnumberGameNumber = Convert.ToInt32(Console.ReadLine());
+
+            // Creating a random game number
+            Random number = new Random();
+            int gameNumber = number.Next(earlynumberGameNumber, endnumberGameNumber);
+
+            // Creation of an endless loop of the game, until it ends
+            for (; ; )
+            {              
+                // Create step loop for the first player
+                Console.WriteLine($"\nStep №{NumbOfStep} \nGame number = {gameNumber} \nPlayer turn: {player1}");
+                while (true)
+                {
+                    // Implementing a set of subtraction numbers
+                    Console.Write($"Enter your subtraction number, {player1}:");
+                    userTry = Convert.ToSByte(Console.ReadLine());
+                    if (userTry > endnumberTry)
+                        Console.WriteLine($"You entered a value greater than the allowed value, enter a number from {earlynumberTry} to {endnumberTry}");
+                    else if (userTry < earlynumberTry)
+                        Console.WriteLine($"You entered a value less than the allowed value, enter a number from {earlynumberTry} to {endnumberTry}");
+                    else
+                    {
+                        // End of turn implementation
+                        gameNumber -= userTry;
+                        NumbOfStep++;
+                        break;
+                    }
+                }
+
+                // Checking the game number and ending the game
+                if (gameNumber <= 0)
+                {
+                    Console.WriteLine($"\nCongratulations, player {player1}! You won {player2} on {NumbOfStep} step");
+                    break;
+                }
+
+                // Create step loop for the second player
+                Console.WriteLine($"\nStep №{NumbOfStep} \nGame number = {gameNumber} \nPlayer turn: {player2}");
+                while (true)
+                {
+                    // Game number check
+                    if (gamemode == 1) // implementation of a step for player
+                    {
+                        // Implementing a set of subtraction numbers
+                        Console.Write($"Enter your subtraction number, {player2}:");
+                        userTry = Convert.ToSByte(Console.ReadLine());
+                        if (userTry > endnumberTry)
+                            Console.WriteLine($"You entered a value greater than the allowed value, enter a number from {earlynumberTry} to {endnumberTry}");
+                        else if (userTry < earlynumberTry)
+                            Console.WriteLine($"You entered a value less than the allowed value, enter a number from {earlynumberTry} to {endnumberTry}");
+                        else
+                        {
+                            // End of turn implementation
+                            gameNumber -= userTry;
+                            NumbOfStep++;
+                            break;
+                        }
+                    }
+                    else  // implementation of a step for PC
+                    {
+                        userTry = number.Next(earlynumberTry, endnumberTry);
+                        gameNumber -= userTry;
+                        NumbOfStep++;
+                        break;
+                    }
+                }
+
+                // Checking the game number and ending the game
+                if (gameNumber <= 0)
+                {
+                    Console.WriteLine($"\nCongratulations, player {player2}! You won {player1} on {NumbOfStep} step");
+                    break;
+                }
+
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # My-first-game-on-C-
 Hallo everyone!I am junior programmer and that my first game. I will be glad to absolutely any criticism, or advice on the code (:
+
+A game that can be played by two players.
+
+At the start, players are asked to enter their nicknames, or select the game mode against the computer.
+Nicknames are stored until the end of the game.
+
+The program guesses a random number gameNumber, the interval of which the player provides.
+
+Players take turns (the game reports the current player's progress)
+
+The player whose turn is indicated enters the number userTry, which can take the value that the player has prescribed, the entered number is subtracted from the gameNumber
+
+The new value for gameNumber is shown to the players on the screen.
+
+The winner is the player, after whose move gameNumber turns to zero.
